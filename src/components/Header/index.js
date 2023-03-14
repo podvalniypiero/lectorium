1e5dee983dafbabb21e125bcb318a228bfbbeb1c
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-03-14 22:41:15 +0300

    fix main scss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,33 +10,35 @@ function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
       <Link to="/">
-        <div className= {styles.headerTop}>
-          <img width={45} height={45} src="img/logotype.svg" alt="Logotype" />
-          <div>
-          <h3 className="text-uppercase">ЛЕКТОРИЙ</h3>
+        <div className={styles.headerTop}>
+          <img width={55} height={55} src="img/logotype.svg" alt="Logotype" className='mr-30' />
+        <div>
+        <div>
+        <h3 className="text-uppercase">ЛЕКТОРИЙ</h3>
         <p className='opacity-5'> ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
+        </div>
         
-        
-        <nav className='d-flex mt-10 flex-row justify-around align-end flex-wrap'>
-          <div className=' '></div>
+        <nav className='d-flex mt-10 flex-row justify-around flex-wrap'>
+          <div className=''>
           <Link to="/favorites">
-            <button className="mr-30 p-15">ИЗБРАННОЕ 
+            <button className="mr-30 p-15 m-5">ИЗБРАННОЕ 
             <img className='ml-5 mr-0' width={13} height={13} src="img/heart.svg" alt="Закладки" />
             </button>
           </Link>
        
           <Link to="/">
-            <button className="mr-30 p-15">О НАС</button>
+            <button className="mr-30 p-15 m-5">О НАС</button>
           </Link>
 
           <Link to="/">
-            <button className="mr-30 p-15">НАША КОЛЛЕКЦИЯ</button>
+            <button className="mr-30 p-15 m-5">НАША КОЛЛЕКЦИЯ</button>
           </Link>
                  
           <Link to="/games">
-             <button className="mr-30 p-15">РАЗДЕЛ ИНТЕРАКТИВА</button>
+             <button className="mr-30 p-15 m-5">РАЗДЕЛ ИНТЕРАКТИВА</button>
           </Link>
-                 
+          </div>
+      
         </nav>
           </div>
         </div>
