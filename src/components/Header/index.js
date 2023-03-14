cb9b8d912d49d59339dda9607b92a24d6e94fdb9
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-03-14 18:04:07 +0300

    styles for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,36 +5,36 @@ import { useCart } from '../../hooks/useCart';
 import styles from './Header.module.scss';
 
 function Header(props) {
-  const { totalPrice } = useCart();
+ 
 
   return (
     <header className="d-flex justify-between align-center p-40">
       <Link to="/">
-        <div className="d-flex align-center">
+        <div className= {styles.headerTop}>
           <img width={45} height={45} src="img/logotype.svg" alt="Logotype" />
           <div>
           <h3 className="text-uppercase">ЛЕКТОРИЙ</h3>
         <p className='opacity-5'> ПРОВОДНИК В МИР ИСКУССТВА СЛОВА</p>
         
         
-        <nav className='d-flex'>
-          <div className='buttons justify-around'></div>
+        <nav className='d-flex mt-10 flex-row justify-around align-end flex-wrap'>
+          <div className=' '></div>
           <Link to="/favorites">
-            <button className="mr-30 p-10 cu-p ">ИЗБРАННОЕ 
+            <button className="mr-30 p-15">ИЗБРАННОЕ 
             <img className='ml-5 mr-0' width={13} height={13} src="img/heart.svg" alt="Закладки" />
             </button>
           </Link>
        
           <Link to="/">
-            <button className="mr-30 p-10 cu-p">О НАС</button>
+            <button className="mr-30 p-15">О НАС</button>
           </Link>
 
           <Link to="/">
-            <button className="mr-30 p-10 cu-p">НАША КОЛЛЕКЦИЯ</button>
+            <button className="mr-30 p-15">НАША КОЛЛЕКЦИЯ</button>
           </Link>
                  
           <Link to="/games">
-             <button className="mr-30 p-10 cu-p">РАЗДЕЛ ИНТЕРАКТИВА</button>
+             <button className="mr-30 p-15">РАЗДЕЛ ИНТЕРАКТИВА</button>
           </Link>
                  
         </nav>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,53 +10,53 @@ function Main() {
 
 
   return (
-          <div className = 'container'>
-            <div className = 'paddingContainer'>
-            <p>ЛЕКТОРИЙ  — ЭТО УНИКАЛЬНЫЙ ПРОЕКТ
-              <br/>ВАШ НАВИГАТОР В МИРЕ ЛИТЕРАТУРЫ
-              <br/>ИНСТРУМЕНТ ПОЗНАНИЯ ИСКУССТВА СЛОВА И САМОГО СЕБЯ
-            </p>
-            <img src='\img\mainIntro.svg' alt='Поколения Лектория' width={350}/>
-          </div> 
-          <div className='aboutContainer'>
-              <div className='paddingAbout'>
-                <div className='logo'>
-                <p>ЛЕКТОРИЙ</p>
-                <p>ЛЮБИМЫЙ</p>
-                <p>ЛИТЕРАТУРНЫЙ</p>
-              </div>
-              
-              
-              <div className='aboutPhoto'>
-                  <img src='\img\main1.jpg' alt='Поколения Лектория' width={350}/>
-                  <p>В НАЧАЛЕ БЫЛО СЛОВО...</p>
-                  <p>ЛЕКТОРИЙ ОБЪЕДИНЯЕТ ПОЭЗИЮ И ПРОЗУ
-                    <br/>КАК ЖИЗНЬ ОБЪЕДИНЯЕТ В СЕБЕ КОМЕДИЮ И ДРАМУ</p>
-              </div>
-              
-              
-              <div className='slider'>
-                <img src='\img\backArrow.svg' alt='Назад'/>
-                <img src='\img\slide.jpg' alt='Литературный слайдер. Маяковский.' width={400}/>
-                <img src='\img\nextArrow.svg' alt='Вперед'/>
-              </div>
+          
+    <div className = 'pos-r w100p mt-180'>
+    <div className='d-flex flex-row justify-center align-center p-20'>
+    <p className='align-center p-30'>ЛЕКТОРИЙ  — ЭТО УНИКАЛЬНЫЙ ПРОЕКТ
+      <br/>ВАШ НАВИГАТОР В МИРЕ ЛИТЕРАТУРЫ
+      <br/>ИНСТРУМЕНТ ПОЗНАНИЯ ИСКУССТВА СЛОВА И САМОГО СЕБЯ
+    </p>
+    <img src='\img\mainIntro.svg' alt='Поколения Лектория' width={350}/>
+  </div> 
+  <div className='w100p pos-r mt-30px'>
+      <div className='paddingAbout'>
+      <div className='d-flex flex-row justify-around'>
+        <p>ЛЕКТОРИЙ</p>
+        <p>ЛЮБИМЫЙ</p>
+        <p>ЛИТЕРАТУРНЫЙ</p>
+      </div>
+        
+      <div className='d-flex flex-column align-center'>
+          <img src='\img\main1.jpg' className = 'p-20' alt='Поколения Лектория' width={350}/>
+          <p>В НАЧАЛЕ БЫЛО СЛОВО...</p>
+          <p>ЛЕКТОРИЙ ОБЪЕДИНЯЕТ ПОЭЗИЮ И ПРОЗУ
+            <br/>КАК ЖИЗНЬ ОБЪЕДИНЯЕТ В СЕБЕ КОМЕДИЮ И ДРАМУ</p>
+      </div>
+      
+      
+      <div className='d-flex flex-row justify-center'>
+        <img className='p-25' src='\img\slider\backArrow.svg' alt='Назад'/>
+        <img className='p-25' src='\img\slider\slide.jpg' alt='Литературный слайдер. Маяковский.' width={400}/>
+        <img className='p-25' src='\img\slider\nextArrow.svg' alt='Вперед'/>
+      </div>
 
-              <div className='aboutPhoto'>
-                <p>ЛИТЕРАТУРА — ЭТО ШЕПОТ ИСТОРИИ
-                  <br/>ЛИТЕРАТУРА — ЭТО ГОЛОС ПОКОЛЕНИЙ
-                  <br/>ЛИТЕРАТУРА — ЭТО МУЗЫКА ВРЕМЕНИ
-                  <br/><br/>ЛИТЕРАТУРА ОБЪЕДИНЯЕТ
-                  <br/>ЛЕКТОРИЙ ПОМОГАЕТ
-                </p>
-                <img src='\img\main2.jpg' alt='Помощь' width={330}/>
-                <div className='aboutBenefits'>
-                  <h3>ПОЧЕМУ ЛЕКТОРИУМ?</h3>
-                </div>
-              </div>
-              
-             </div>
-            </div>
-            </div>
+      <div className='d-flex flex-column align-center'>
+        <p>ЛИТЕРАТУРА — ЭТО ШЕПОТ ИСТОРИИ
+          <br/>ЛИТЕРАТУРА — ЭТО ГОЛОС ПОКОЛЕНИЙ
+          <br/>ЛИТЕРАТУРА — ЭТО МУЗЫКА ВРЕМЕНИ
+          <br/><br/>ЛИТЕРАТУРА ОБЪЕДИНЯЕТ
+          <br/>ЛЕКТОРИЙ ПОМОГАЕТ
+        </p>
+        <img className='p-20' src='\img\main2.jpg' alt='Помощь' width={330}/>
+        <div className= 'p-35 w70p'>
+          <h3 className='d-flex justify-center p-20'>ПОЧЕМУ ЛЕКТОРИУМ?</h3>
+        </div>
+      </div>
+      
+     </div>
+    </div>
+    </div>
                
   );
 }
