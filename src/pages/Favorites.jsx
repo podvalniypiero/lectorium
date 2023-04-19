dbd0238fdcf11784b1d5d72ae04e72f3226c19cc
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-04-19 19:01:23 +0300

    noFavourites variant added

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import AppContext from '../context';
 import styles from './Pages.module.scss'; 
@@ -14,26 +15,31 @@ function Favorites() {
       </div>
 
       <div className="d-flex justify-center flex-wrap mb-60">
-        
-        {/* {  Array(favorites).length === 1  ? 
-        
-         <div className={styles.noFavorites}> 
-            <div className={styles.containerDark}>
-              <h3 className='p-20'>ВЫ ЕЩЕ НЕ ДОБАВИЛИ НИЧЕГО В <span>ИЗБРАННОЕ</span></h3>
-              <img src='\img\noFavorites.jpg' alt='Вы еще не добавили ничего в Избранное' className='mb-20' width={450}/>
-              <h3>ПЕРЕЙТИ В СЛОВАРЬ <span>ЛЕКТОРИЯ</span></h3>
-              <h3>ПРОСМОТРЕТЬ АВТОРОВ <span>ЛЕКТОРИЯ</span></h3>
-
+        {
+          favorites.length > 0 ? 
+          (
+          <>{favorites.map((item, index) => (
+            <Card key={index} favorited={true} onFavorite={onAddToFavorite} {...item} />
+          ))}</> 
+          ) 
+          :
+          (
+            <div className={styles.noFavorites}> 
+              <div className={styles.containerDark}>
+                <h3 className='p-20'>ВЫ ЕЩЕ НЕ ДОБАВИЛИ НИЧЕГО В <span>ИЗБРАННОЕ</span></h3>
+                <img src='\img\noFavorites.jpg' alt='Вы еще не добавили ничего в Избранное' className='mb-20' width={450}/>
+                
+                <Link to="/vocabulary">
+                  <h3>ПЕРЕЙТИ В СЛОВАРЬ <span>ЛЕКТОРИЯ</span></h3>
+                </Link>
+                <Link to="/authors">
+                  <h3>ПРОСМОТРЕТЬ АВТОРОВ <span>ЛЕКТОРИЯ</span></h3>
+                </Link>
+              </div>
             </div>
-          </div>
-
-         :  favorites.map((item, index) => (
-          <Card key={index} favorited={true} onFavorite={onAddToFavorite} {...item} />
-        ))} 
-        */}
-         {favorites.map((item, index) => (
-          <Card key={index} favorited={true} onFavorite={onAddToFavorite} {...item} />
-        ))}
+          )
+        }
+         
       </div>
     </div>
   );
